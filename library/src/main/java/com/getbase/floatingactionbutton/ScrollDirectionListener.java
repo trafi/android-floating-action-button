66a97539508fde8955c0230e9b605483ee94b5f2
Simonas Joris Samaitis <simonas@trafi.com>
2015-11-09 15:23:12 +0200

    Added hide on scroll for ScrollView

---
+++ library/src/main/java/com/getbase/floatingactionbutton/ScrollDirectionListener.java
@@ -0,0 +1,10 @@
+package com.getbase.floatingactionbutton;
+
+/**
+ * Created by Simas on 11/9/2015.
+ */
+public interface ScrollDirectionListener {
+    void onScrollDown();
+
+    void onScrollUp();
+}
